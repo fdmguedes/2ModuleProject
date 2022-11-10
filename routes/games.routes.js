54db1e01149f60e598da8be431753271ea586d89
MeerKat <98255504+MeerKatPT@users.noreply.github.com>
2022-11-10 16:19:00 +0000

    project done

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User.model");
 
 //.get to display the games (straight return because we are only rendering === no promises)
-router.get("/games", (req, res, next) => res.render("categories/games"));
+router.get("/games", async (req, res, next) => {
+  const currentUser = req.session.currentUser;
+  res.render("categories/games", { currentUser });
+});
 
 module.exports = router;
